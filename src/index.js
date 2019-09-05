6dee0eafe9d0db72a01b8dc6a1c546219efd8be4
Wang Sijie <i@sijie.wang>
2019-09-05 19:20:24 +0800

    fix multi loading bug

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
-import { message } from 'antd';
+import { message } from "antd";
 
 let hideIndicator = null;
 let request = 0;
 let differTime = 300;
-let defaultText = 'Loading';
+let defaultText = "Loading";
 
-export const pushRequest = (text) => {
-    if (request === 0) {
-        hideIndicator = null;
-        setTimeout(() => {
-            if (request > 0) {
-                hideIndicator = message.loading(text || defaultText, 0);
-            }
-        }, differTime);
-    }
-    request += 1;
+export const pushRequest = text => {
+  if (request === 0) {
+    setTimeout(() => {
+      if (request > 0 && !hideIndicator) {
+        hideIndicator = message.loading(text || defaultText, 0);
+      }
+    }, differTime);
+  }
+  request += 1;
 };
 
 export const popRequest = () => {
-    request -= 1;
+  if (request === 1) {
     hideIndicator && hideIndicator();
+    hideIndicator = null;
+  } else if (request <= 0) {
+    return (request = 0);
+  }
+  request -= 1;
 };
 
 export const config = (_defaultText, _differTime = 300) => {
-    defaultText = _defaultText;
-    differTime = _differTime;
+  defaultText = _defaultText;
+  differTime = _differTime;
 };
